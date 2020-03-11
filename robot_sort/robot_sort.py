10efe06825662b6c57423c6c292c5e53b27b1a9e
ibomiddleton <46385886+ibomiddleton@users.noreply.github.com>
2020-03-10 20:35:39 -0400

    Update robot_sort.py

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -126,27 +126,6 @@ class SortingRobot:
                         self.swap_item()
                         break
         pass 
-        # self.set_light_on()
-        
-        # self.swap_item()
-        
-        # while self.light_is_on():
-            
-        #     while self.move_right():
-        #         if self.compare_item() == 1:
-        #             self.swap_item()
-        #     if self.compare_item() is None:
-        #         self.swap_item()
-        #         self.set_light_off()
-        #         break
-        #     else:
-        #         while self.move_left():
-        #             if self.compare_item() is None:
-        #                 self.swap_item()
-        #                 self.move_right()
-        #                 self.swap_item()
-        #                 break     
-        # pass
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
